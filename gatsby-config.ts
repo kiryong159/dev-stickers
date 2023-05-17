6ce9d9ac19fb3db5f55863d9179e1bbbb7548d87
kiryong <116635003+kiryong159@users.noreply.github.com>
2023-05-17 10:43:56 +0900

    10.11

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,17 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        // Path to the directory
+        path: `${__dirname}/blog-posts/`,
+      },
+    },
+  ],
 };
 
 export default config;
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,16 +1,44 @@
+import { graphql, PageProps } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
 
-function Blog() {
+function Blog({ data }: PageProps<Queries.BlogPostsQuery>) {
+  console.log(data);
   return (
     <Layout title="welcome by blog">
-      <div>
-        <p>sd </p>
-      </div>
+      <section>
+        {data.allMdx.nodes.map((post, index) => (
+          <article key={index}>
+            <h3>{post.frontmatter?.title}</h3>
+            <h5>
+              {post.frontmatter?.author} in : {post.frontmatter?.category}
+            </h5>
+            <h6>{post.frontmatter?.date}</h6>
+            <hr />
+            <p>{post.excerpt}</p>
+          </article>
+        ))}
+      </section>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogPosts {
+    allMdx {
+      nodes {
+        frontmatter {
+          author
+          category
+          title
+          date(formatString: "YYYY.MM.DD")
+        }
+        excerpt
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
 
